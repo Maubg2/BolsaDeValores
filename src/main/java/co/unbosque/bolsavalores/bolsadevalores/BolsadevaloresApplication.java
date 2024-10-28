468bba981c02e9d6b5f63144d79f53a4a8baf588
Maubg2 <mauriciobel2002@gmail.com>
2024-10-27 23:32:38 -0500

    Configurar properties

---
+++ src/main/java/co/unbosque/bolsavalores/bolsadevalores/BolsadevaloresApplication.java
@@ -0,0 +1,13 @@
+package co.unbosque.bolsavalores.bolsadevalores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsadevaloresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsadevaloresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/unbosque/bolsavalores/bolsadevalores/BolsadevaloresApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unbosque.bolsavalores.bolsadevalores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsadevaloresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
